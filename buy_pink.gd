61151196ab90dd50857fab1dcfca8bcba4a005d0
littleinventor7 <58573187+littleinventor7@users.noreply.github.com>
2026-06-06 23:15:54 +0300

    Add files via upload

---
+++ buy_pink.gd
@@ -0,0 +1,9 @@
+extends Area2D
+func _input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	# Check if the event is a mouse button click
+	if event is InputEventMouseButton:
+		# Check if it's the left button and if it was pressed
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed  :
+			if Global.coins >=10:
+				Global.coins -= 10
+				Global.chracter_pink = 1
